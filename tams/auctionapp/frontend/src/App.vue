e38769688be65670ac3e86fbbe4415334dc88ec5
Syeda Sadia Taznim <60143096+sadia-tzm@users.noreply.github.com>
2022-11-22 19:05:08 +0000

    Revert "navbar and vue-router development"
    This reverts commit 21b80416fb608941de5be6f9248416febaec6f6f.

--- tams/auctionapp/frontend/src/App.vue
+++ tams/auctionapp/frontend/src/App.vue
@@ -1,20 +1,31 @@
-<template>
-  <NavBar />
-  <div :style="{ 'margin-left': navbarWidth }">
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
 
-    <router-view />
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
   </div>
+  <HelloWorld msg="Vite + Vue" />
 </template>
 
-
-<script lang="ts">
-import NavBar from './components/NavBar/NavBar.vue';
-import { navbarWidth } from './components/NavBar/state';
-export default {
-  components: { NavBar },
-  setup() {
-    return { navbarWidth }
-  }
-
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
 }
-</script>
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
--- tams/auctionapp/frontend/src/main.ts
+++ tams/auctionapp/frontend/src/main.ts
@@ -1,8 +1,5 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap"
-import '@fortawesome/fontawesome-free/js/all'
 
 createApp(App).mount('#app')
