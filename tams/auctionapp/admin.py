74fbb59c79d6c347f72e7d14693aefd14b02ff75
Aysha Uddin <aysha.uddin@kpmg.co.uk>
2022-11-26 18:07:52 +0000

    database-v2: Create User model

--- tams/auctionapp/admin.py
+++ tams/auctionapp/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import User
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+class UserAdminConfig(UserAdmin):
+    search_fields = ('email', 'username', 'first_name', 'last_name',)
+    ordering = ('-date_of_birth',)
+    list_display = ('email', 'username', 'first_name', 
+    'last_name', 'is_active')
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'password1', 'password2',)}),
+    )
+
+admin.site.register(User, UserAdminConfig)
--- tams/auctionapp/models.py
+++ tams/auctionapp/models.py
@@ -1,3 +1,67 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import AbstractUser, PermissionsMixin, UserManager
 
-# Create your models here.
+class CustomAccountManager(UserManager):
+
+    def create_superuser(self, email: str, username: str, first_name: str, last_name: str, password: str, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(
+                'Superuser must be assigned to is_staff=True'
+            )
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(
+                'Superuser must be assigned to is_superuser=True'
+            )
+
+        return self.create_user(email, username, first_name, last_name, password, **extra_fields)
+    
+    def create_user(self, email: str, username: str, first_name: str, last_name: str, password: str, **extra_fields):
+        if not email:
+            raise ValueError(_('You must provide an email address'))
+
+        email = self.normalize_email(email)
+        extra_fields.setdefault('is_active', True)
+
+        user = self.model(email=email, username=username,
+                            first_name=first_name, last_name = last_name,
+                            **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+
+class User(AbstractUser, PermissionsMixin):
+    '''
+    Our User models is a sub-class of Django's AbstractUser
+    So we can make use of Django's authentication system
+    '''
+
+    email = models.EmailField(_('email address'), unique=True)
+    username = models.CharField(
+        _('username'),
+        max_length=150,
+        unique=True,
+        help_text=_('Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.'),
+        db_column='user_name'  # <------------------------
+    )
+    first_name = models.CharField(max_length=150, blank=True)
+    last_name = models.CharField(max_length=150, blank=True)
+    date_of_birth = models.DateField(auto_now=True)
+    image = models.ImageField(upload_to='images', default='default.png')
+
+    is_staff = models.BooleanField(default=False)
+    is_active: models.BooleanField(default=False)
+
+    objects = CustomAccountManager()
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    def __str__(self):
+        return self.username
+
