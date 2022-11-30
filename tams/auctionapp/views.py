9ca35deb5501f0654ac64f063c3f9b7ed08bb28b
tazmena <78047816+tazmena@users.noreply.github.com>
2022-11-30 17:32:31 +0000

    Beginning of login and sign up

--- tams/auctionapp/views.py
+++ tams/auctionapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+#def index(request):
+ #   return HttpResponse("Hello, world. You're at the auction app index.")
+
+def login(request):
+    return render(request, 'auctionapp/login.html')
+
+def signup(request):
+    return render(request, 'auctionapp/signup.html')
