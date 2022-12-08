60020d492c50d0dbe7194f26f6da1a1737d9aa1f
Aysha Uddin <aysha.uddin@kpmg.co.uk>
2022-12-08 19:29:49 +0000

    edit-profile: Add upload button

--- tams/auctionapp/frontend/src/views/Profile.vue
+++ tams/auctionapp/frontend/src/views/Profile.vue
@@ -7,8 +7,46 @@
 					class="rounded-circle img-thumbnail mx-auto d-block profile-photo"
 					alt="User-Profile-Image"
 				/>
+				<div v-if="edit">
+					<button type="button" class="profile-pic-button btn-lg">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							xmlns:xlink="http://www.w3.org/1999/xlink"
+							enable-background="new 0 0 32 32"
+							height="20px"
+							id="Layer_1"
+							version="1.1"
+							viewBox="0 0 32 32"
+							width="20px"
+							xml:space="preserve"
+							class="btn-icon"
+						>
+							<g id="camera">
+								<path
+									clip-rule="evenodd"
+									d="M16,10.001c-4.419,0-8,3.581-8,8c0,4.418,3.581,8,8,8   c4.418,0,8-3.582,8-8C24,13.583,20.418,10.001,16,10.001z M20.555,21.906c-2.156,2.516-5.943,2.807-8.459,0.65   c-2.517-2.156-2.807-5.944-0.65-8.459c2.155-2.517,5.943-2.807,8.459-0.65C22.42,15.602,22.711,19.391,20.555,21.906z"
+									fill="#ffffff"
+									fill-rule="evenodd"
+								/>
+								<path
+									clip-rule="evenodd"
+									d="M16,14.001c-2.209,0-3.999,1.791-4,3.999v0.002   c0,0.275,0.224,0.5,0.5,0.5s0.5-0.225,0.5-0.5V18c0.001-1.656,1.343-2.999,3-2.999c0.276,0,0.5-0.224,0.5-0.5   S16.276,14.001,16,14.001z"
+									fill="#ffffff"
+									fill-rule="evenodd"
+								/>
+								<path
+									clip-rule="evenodd"
+									d="M29.492,9.042l-4.334-0.723l-1.373-3.434   C23.326,3.74,22.232,3,21,3H11C9.768,3,8.674,3.74,8.214,4.886L6.842,8.319L2.509,9.042C1.055,9.283,0,10.527,0,12v15   c0,1.654,1.346,3,3,3h26c1.654,0,3-1.346,3-3V12C32,10.527,30.945,9.283,29.492,9.042z M30,27c0,0.553-0.447,1-1,1H3   c-0.553,0-1-0.447-1-1V12c0-0.489,0.354-0.906,0.836-0.986l5.444-0.907l1.791-4.478C10.224,5.25,10.591,5,11,5h10   c0.408,0,0.775,0.249,0.928,0.629l1.791,4.478l5.445,0.907C29.646,11.094,30,11.511,30,12V27z"
+									fill="#ffffff"
+									fill-rule="evenodd"
+								/>
+							</g>
+						</svg>
+						<p class="d-inline btn-text">Add profile picture</p>
+					</button>
+				</div>
 			</div>
-			<h2 class="card-text text-white font-weight-bold m-t-40">{{ fullname }}</h2>
+			<h2 class="card-text text-white font-weight-bold m-t-40 name">{{ fullname }}</h2>
 			<p class="card-text text-muted">@{{ username }}</p>
 		</div>
 		<div class="col-md-7 bg-light rounded">
@@ -246,9 +284,30 @@ body {
 	margin-right: auto;
 }
 
+.profile-pic-button {
+	width: 70%;
+	background: transparent !important;
+	border: 1px solid #e0e0e0;
+	color: white;
+	margin-top: 20px;
+}
+
+.name {
+	margin-top: 80px !important;
+}
+
 .btn {
 	background-color: #c59fc9;
 	color: white;
+}
+
+.btn-icon {
+	margin-right: 5px;
+	margin-bottom: 5px;
+}
+
+.btn-text {
+	margin-left: 5px;
 }
 
 .btn:hover {
