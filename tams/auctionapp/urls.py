384f615e13768a004078009190a705b6542da02d
Syeda Sadia Taznim <60143096+sadia-tzm@users.noreply.github.com>
2022-12-09 15:42:27 +0000

    fixing merge conflict

--- tams/auctionapp/urls.py
+++ tams/auctionapp/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('signup', views.signup, name='signup'),
     path('api/profile/<int:user_id>/', profile_api, name="profile api"),
     path('api/products/', views.fetch_products, name='fetch_products'),
-    path('api/products/<int:product_id>/', views.fetch_products_id, name='fetch_product_id'),
+    ## path('api/products/<int:product_id>/', views.fetch_products_id, name='fetch_product_id'),
     path('items/<int:product_id>', views.product_details, name='product_details')
     ## path('api/search/', api.api_search, name='api_search'),
 ]
