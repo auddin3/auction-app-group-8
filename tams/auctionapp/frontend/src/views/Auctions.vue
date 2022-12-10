0ad4be29d28d21c8ee8e5fa357b592a24ec0eb35
Syeda Sadia Taznim <60143096+sadia-tzm@users.noreply.github.com>
2022-12-10 01:59:34 +0000

    Tidying up CSS

--- tams/auctionapp/frontend/src/views/Auctions.vue
+++ tams/auctionapp/frontend/src/views/Auctions.vue
@@ -1,27 +1,25 @@
 <template>
-  <div class="auctions"
-    style="background-color:#C59FC9; padding: 10px; border-radius: 10px; width: 100%; margin:auto; color:white;">
+  <div class="auctions">
     <h1 class="">Auctions</h1>
   </div>
 
-  <div id="search" style="">
-    <div style="background-color: #CFBAE1; width: 60%; margin: auto; border-radius: 10px;" class="p-4 mt-4">
-      <h3 class="" style="color:white">Search</h3>
-      <input type="text" v-model="search" class="" style="background-color: white; color: black;" />
+  <div id="search">
+    <div class="search-box p-4 mt-4">
+      <h3 class="search-heading">Search</h3>
+      <input type="text" v-model="search" class="input-box" />
     </div>
   </div>
 
   <div v-for="product in filteredProducts">
     <div class="card-group">
       <div class="card mt-4" style="width: 10rem;">
-        <img class="card-img-top" src="../assets/vue.svg" style="height: 200px; width: 200px; margin:auto;"
-          alt="Item image" />
+        <img class="card-img-top product-image" src="../assets/vue.svg" alt="Item image" />
       </div>
       <div class="card mt-4" style="width: 60rem;">
         <div class="card-body">
-          <h4 class="card-title" style="">{{ product['product_name'] }}</h4>
+          <h4 class="card-title">{{ product['product_name'] }}</h4>
           <p class="card-text">{{ product['description'] }}</p>
-          <p class="card-text" style="color:#7354B5"><strong>Start Price: £{{ product['start_price'] }}</strong></p>
+          <p class="card-text price-colour"><strong>Start Price: £{{ product['start_price'] }}</strong></p>
           <p class="card-text">End of Bid: {{ product['end_of_bid'] }}</p>
           <p class="card-text">Owner: {{ product['owner'] }}</p>
           <button @click=view_item_details(product) class="btn btn-secondary">View Item Details</button>
@@ -44,18 +42,6 @@ export default {
       products: [],
       query: '',
       search: "",
-      products2: [
-        {
-          id: 1,
-          title: "iphone",
-          body: "iphone"
-        },
-        {
-          id: 2,
-          title: "laptop",
-          body: "hp"
-        }
-      ]
     };
   },
   computed: {
@@ -92,3 +78,58 @@ export default {
   }
 }
 </script>
+
+<style>
+body {
+	background-color: #a4def9;
+}
+
+.btn {
+	background-color: #c59fc9;
+	color: white;
+}
+
+.btn:hover {
+	background-color: #c1e0f7;
+}
+
+.input-box:hover {
+	background-color: #e8e8e8;
+}
+
+.input-box {
+  background-color: white;
+  color: black;
+}
+
+.auctions {
+  background-color:#C59FC9;
+  padding: 10px;
+  border-radius: 10px;
+  width: 100%;
+  margin:auto;
+  color:white;
+}
+
+.search-box {
+  background-color: #CFBAE1;
+  width: 60%;
+  margin: auto;
+  border-radius: 10px;
+}
+
+.search-heading {
+  color:white;
+}
+
+.product-image {
+  height: 200px;
+  width: 200px;
+  margin:auto;
+}
+
+.price-colour {
+  color:#7354B5
+}
+
+</style>
--- tams/auctionapp/frontend/src/views/Inventory.vue
+++ tams/auctionapp/frontend/src/views/Inventory.vue
@@ -9,43 +9,16 @@
 export default {
   data() {
     return {
-      query: '',
-      products: [],
     };
   },
-  delimiters: ['[[', ']]'],
   methods: {
-    // async fetch_products() {
-    //   let response = await fetch("http://127.0.0.1:8000/auctionapp/api/products/");
-    //   let data = await response.json();
-    //   this.products = data.products;
-    // },
-    performSearch() {
-      var data = {
-        'query': this.query,
-      }
-      fetch('/auctionapp/api/search/', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          //  no csrf token line
-          // 'X-CSRFToken': {{ csrf_token }}
-        },
-        body: JSON.stringify(data)
-      })
-      .then((response) => {
-        console.log(data)
-        return response.json()
-      })
-      .then((result) => {
-        console.log(result.products)
-        this.products = result.products
-      })
-      .catch((error) => {
-        console.log('Error', error)
-      })
-    },
   },
   components: { }
 }
 </script>
+
+<style>
+body {
+	background-color: #a4def9;
+}
+</style>
