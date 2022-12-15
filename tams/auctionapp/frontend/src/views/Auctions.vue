4a9da93b61141647f2ca8e57703e17153a3220ad
Aysha Uddin <aysha.uddin@kpmg.co.uk>
2022-12-15 12:09:25 +0000

    view-bids - show different times for each product

--- tams/auctionapp/frontend/src/views/Auctions.vue
+++ tams/auctionapp/frontend/src/views/Auctions.vue
@@ -1,106 +1,123 @@
 <template>
-  <div class="auctions">
-    <h1 class="">Auctions</h1>
-  </div>
+	<div class="auctions">
+		<h1 class="">Auctions</h1>
+	</div>
 
-  <div id="search">
-    <div class="search-box p-3 mt-4">
-      <!-- <h3 class="search-heading">Search</h3> -->
-      <input type="text" v-model="search" class="input-box p-2" placeholder=" Search Upcoming Bids"/>
-    </div>
-  </div>
+	<div id="search">
+		<div class="search-box p-3 mt-4">
+			<!-- <h3 class="search-heading">Search</h3> -->
+			<input
+				type="text"
+				v-model="search"
+				class="input-box p-2"
+				placeholder=" Search Upcoming Bids"
+			/>
+		</div>
+	</div>
 
-  <div v-for="product in filteredProducts">
-    <div class="card-group">
-      <div class="card mt-4">
-        <div v-if="product.product_image.length > 0" class="img-div">
-           <img class="card-img-top product-image" :src="`http://localhost:8000${product.product_image}`" alt="Item image" />
-        </div>
-        <div v-else class="img-div">
-            <img class="card-img-top product-image" :src="`http://localhost:8000${imgpath}`" alt="Item image" />
-        </div>
-      </div>
-      <div class="card mt-4" style="width: 60rem;">
-        <div class="card-body">
-          <h4 class="card-title">{{ product['product_name'] }}</h4>
-          <p class="card-text">{{ product['description'] }}</p>
-          <p class="card-text price-colour"><strong>Start Price: £{{ product['start_price'] }}</strong></p>
-          <!-- <p class="card-text">End of Bid: {{ product['end_of_bid'] }}</p> -->
-          <button @click=formatTime(product.end_of_bid) class="btn"><strong>Click to view End of Bid deadline</strong></button>
-          <p v-if='endOfBidFormatted != ""'><strong>{{ endOfBidFormatted }} Left</strong></p>
-          <p class="card-text mt-4"><strong>Owner:</strong> {{ product['owner'] }}</p>
-          <button @click=view_item_details(product) class="btn btn-secondary"><strong>View Item Details</strong></button>
-        </div>
-      </div>
-
-    </div>
-  </div>
-
-
+	<div v-for="product in filteredProducts">
+		<div class="card-group">
+			<div class="card mt-4">
+				<div v-if="product.product_image.length > 0" class="img-div">
+					<img
+						class="card-img-top product-image"
+						:src="`http://localhost:8000${product.product_image}`"
+						alt="Item image"
+					/>
+				</div>
+				<div v-else class="img-div">
+					<img
+						class="card-img-top product-image"
+						:src="`http://localhost:8000${imgpath}`"
+						alt="Item image"
+					/>
+				</div>
+			</div>
+			<div class="card mt-4" style="width: 60rem">
+				<div class="card-body">
+					<h4 class="card-title">{{ product["product_name"] }}</h4>
+					<p class="card-text">{{ product["description"] }}</p>
+					<p class="card-text price-colour"
+						><strong>Start Price: £{{ product["start_price"] }}</strong></p
+					>
+					<!-- <p class="card-text">End of Bid: {{ product['end_of_bid'] }}</p> -->
+					<!-- <button @click=formatTime(product.end_of_bid) class="btn"><strong>Click to view End of Bid deadline</strong></button> -->
+					<p v-if="endOfBidFormatted == true"><strong>{{ product.end_of_bid }} Left</strong></p>
+					<p class="card-text mt-4"><strong>Owner:</strong> {{ product["owner"] }}</p>
+					<button @click="view_item_details(product)" class="btn btn-secondary"
+						><strong>View Item Details</strong></button
+					>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script lang="ts">
-import router from '../router';
-import Product from './Product.vue';
+import router from "../router";
+import Product from "./Product.vue";
 
 export default {
-  data() {
-    return {
-      products: [],
-      query: '',
-      search: "",
-      imgpath: "/media/product-images/stock-image.png",
-      endOfBidFormatted: "",
-      noOfSecsLeft: 0,
-    };
-  },
-  computed: {
-    filteredProducts() {
-      const filteredProducts = JSON.parse(JSON.stringify(this.products))
-      return filteredProducts.filter((obj: { product_name: string, description: string }) =>
-        obj.product_name.toLowerCase().includes(this.search.toLowerCase()) 
-        || obj.description.toLowerCase().includes(this.search.toLowerCase()) 
-      );
-    }
-  },
-  methods: {
-    // async fetch_products() {
-    //   let response = await fetch("http://127.0.0.1:8000/auctionapp/api/products/");
-    //   let data = await response.json();
-    //   this.products = data.products;
-    // },
-    view_item_details(product: any) {
-      try {
-        this.$router.push({ name: 'Items', path: '/items/:pid', params: { pid: product.id } })
-      } catch (e) {
-        console.log(e)
-      }
-    },
-    async formatTime(end_of_bid: any){
-      let date = new Date();
-			let endDateTime = new Date(end_of_bid)
-			let noOfSecsLeft = (endDateTime.valueOf()-date.valueOf())/1000; //should return milliseconds left
-			// console.log("no of secs left ",noOfSecsLeft,"s")
+	data() {
+		return {
+			products: [],
+			query: "",
+			search: "",
+			imgpath: "/media/product-images/stock-image.png",
+			endOfBidFormatted: false,
+			noOfSecsLeft: 0,
+		};
+	},
+	computed: {
+		filteredProducts() {
+			const filteredProducts = JSON.parse(JSON.stringify(this.products));
+			return filteredProducts.filter(
+				(obj: { product_name: string; description: string }) =>
+					obj.product_name.toLowerCase().includes(this.search.toLowerCase()) ||
+					obj.description.toLowerCase().includes(this.search.toLowerCase())
+			);
+		},
+	},
+	methods: {
+		// async fetch_products() {
+		//   let response = await fetch("http://127.0.0.1:8000/auctionapp/api/products/");
+		//   let data = await response.json();
+		//   this.products = data.products;
+		// },
+		view_item_details(product: any) {
+			try {
+				this.$router.push({ name: "Items", path: "/items/:pid", params: { pid: product.id } });
+			} catch (e) {
+				console.log(e);
+			}
+		},
+		formatTime(product: { end_of_bid: string | number | Date }) {
+			let date = new Date();
+			let endDateTime = new Date(product.end_of_bid);
+			let noOfSecsLeft = (endDateTime.valueOf() - date.valueOf()) / 1000; //should return milliseconds left
 
-      let seconds = noOfSecsLeft
-			let days = Math.floor(seconds/(24*3600))
-			seconds = seconds % (24*3600)
-			let hours = Math.floor(seconds/3600)
-			seconds = seconds % 3600
-			let minutes = Math.floor(seconds/60)
-			this.endOfBidFormatted = (days+' Days '+hours+' Hours '+minutes+' Minutes ')
-		}
-  },
+			let seconds = noOfSecsLeft;
+			let days = Math.floor(seconds / (24 * 3600));
+			seconds = seconds % (24 * 3600);
+			let hours = Math.floor(seconds / 3600);
+			seconds = seconds % 3600;
+			let minutes = Math.floor(seconds / 60);
+			const val = days + " Days " + hours + " Hours " + minutes + " Minutes ";
+			return val;
+		},
+	},
 
-  async mounted() {
-    let response = await fetch("http://127.0.0.1:8000/auctionapp/api/products/");
-    let data = await response.json();
+	async mounted() {
+		const response = await fetch("http://127.0.0.1:8000/auctionapp/api/products/");
+		let data = await response.json()
+		data.products.forEach((el: { end_of_bid: any }) => (el.end_of_bid = this.formatTime((el))));
     this.products = data.products;
-  },
-  components: {
-    Product
-  }
-}
+    this.endOfBidFormatted = true
+	},
+	components: {
+		Product,
+	},
+};
 </script>
 
 <style>
@@ -122,39 +139,38 @@ body {
 }
 
 .input-box {
-  background-color: white;
-  color: black;
-  border-radius: 10px;
-  width: 90%;
-  border: none;
+	background-color: white;
+	color: black;
+	border-radius: 10px;
+	width: 90%;
+	border: none;
 }
 
 .auctions {
-  background-color:#C59FC9;
-  padding: 10px;
-  border-radius: 10px;
-  width: 100%;
-  margin:auto;
-  color:white;
+	background-color: #c59fc9;
+	padding: 10px;
+	border-radius: 10px;
+	width: 100%;
+	margin: auto;
+	color: white;
 }
 
 .search-box {
-  background-color: #c1e0f7;
-  width: 60%;
-  margin: auto;
-  border-radius: 10px;
+	background-color: #c1e0f7;
+	width: 60%;
+	margin: auto;
+	border-radius: 10px;
 }
 
 .search-heading {
-  color:white;
+	color: white;
 }
 
 .product-image {
-  margin: auto 0;
+	margin: auto 0;
 }
 
 .price-colour {
-  color:#7354B5
+	color: #7354b5;
 }
-
 </style>
